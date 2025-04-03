1a2ea91bfcce134fd859ff5fbbbda399f2bb6782
AnikaTabassum217 <tabassum.anika579@gmail.com>
2025-04-03 21:19:18 +0600

    Image showing in Bottle Page

--- src/components/Bottles/Bottles.jsx
+++ src/components/Bottles/Bottles.jsx
@@ -1,10 +1,27 @@
 import React, { use } from 'react';
+import Bottle from '../Bottle/Bottle';
 
 const Bottles = ({bottlesPromise}) => {
+
     const bottles = use(bottlesPromise)
-    console.log(bottles)
+    // console.log(bottles)
+
     return (
         <div>
+            {bottles.length}
+
+            {/* <div>
+                {bottles.map((bottle,index)=>{
+                    return(
+                        <div key={index}>
+                        {bottle.id}
+                        </div>
+                    )
+                })}
+            </div> */}
+
+
+            {bottles.map(bottle=> <Bottle key={bottle.id} bottle={bottle}></Bottle>)}
             
         </div>
     );
